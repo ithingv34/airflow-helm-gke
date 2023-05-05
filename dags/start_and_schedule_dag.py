37883b2ebafa73ae1ddb3a0f78d53fccb26a8e17
ithingv34 <ithingv34@gmail.com>
2023-05-06 02:11:45 +0900

    dummyopearotr

---
+++ dags/start_and_schedule_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'start_date': datetime(2023, 5, 1)
+    'owner': 'Airflow'
+}
+
+with DAG(
+    dag_id='start_and_schedule_dag',
+    schedule_interval="0 * * * *",
+    default_args=default_args
+) as dag:
+    
+    # Task 1
+    dummy_task = DummyOperator(
+        task_id='dummy_task1'
+    )
+
+    # Task 2
+    dummy_task = DummyOperator(
+        task_id='dummy_task2'
+    )
+
+    dummy_task1 >> dummy_task2
+
+    
