a4ac300d939fc521f9e583cb19925df5a0068ffb
ithingv34 <ithingv34@gmail.com>
2023-05-06 22:22:45 +0900

    add_dags.py

--- dags/project_a/project_a.py
+++ dags/project_a/project_a.py
@@ -1,16 +1,19 @@
-# Script to add new DAGs folders using 
-# the class DagBag
-# Paths must be absolute
-import os
-from airflow.models import DagBag
-dags_dirs = [
-                '/opt/airflow/dags/project_a', 
-                '/opt/airflow/dags/project_b'
-            ]
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
 
-for dir in dags_dirs:
-   dag_bag = DagBag(os.path.expanduser(dir))
+from datetime import datetime, timedelta
 
-   if dag_bag:
-      for dag_id, dag in dag_bag.dags.items():
-         globals()[dag_id] = dag
+default_args = {
+    'start_date': datetime(2023, 5, 1),
+    'owner': 'Airflow'
+}
+
+with DAG(dag_id='project_a', schedule_interval="0 0 * * *", default_args=default_args, catchup=False) as dag:
+    
+    # Task 1
+    bash_task_1 = BashOperator(task_id='bash_task_1', bash_command="echo 'first task'")
+    
+    # Task 2
+    bash_task_2 = BashOperator(task_id='bash_task_2', bash_command="echo 'second task'")
+
+    bash_task_1 >> bash_task_2
